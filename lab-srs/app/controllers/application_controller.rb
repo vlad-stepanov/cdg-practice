94862d7943f6473811bed3e7696d26c4ebd0ced8
Vladislav Stepanov <ambulanc3.dev@gmail.com>
2022-01-10 04:21:08 +0300

    [LAB-7]: Laboratory work 7 is done

--- lab-srs/app/controllers/application_controller.rb
+++ lab-srs/app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
-end
+    before_action :require_login
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  
+    private
+  
+    def require_login
+      unless user_signed_in?
+        flash[:error] = "You must be logged in to access this section"
+        authenticate_user!
+        # redirect_to new_user_session_path
+      end
+    end
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :first_name, :last_name])
+    end
+  
+    def after_sign_out_path_for(resourse_or_scope)
+      request.referrer
+    end
+  end
--- lab-srs/db/schema.rb
+++ lab-srs/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_07_195733) do
+ActiveRecord::Schema.define(version: 2022_01_09_213501) do
 
   create_table "reports", force: :cascade do |t|
     t.integer "user_id"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 2021_12_07_195733) do
     t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
